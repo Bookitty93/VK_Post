6315102e6ae4f97df9427e01e164c2f31ff90f95
Anvar Mubashirov <anvar.mubashirov@mail.ru>
2023-06-07 15:25:29 +0500

    Fixed WallService commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,39 @@
 fun main() {
-    println(WallService.add(Post(1, 1, "Hi", "Test", 1, 1, 1, canPin = true, canEdit = true)))
-    println(WallService.upDate(Post(1, 2, "Hello", "tesst2", 2, 2, 2, canPin = false, canEdit = false)))
+    println(
+        WallService.add(
+            Post(
+                1,
+                1,
+                "Hi",
+                "Test",
+                1,
+                1,
+                1,
+                canPin = true,
+                canEdit = true,
+                comments = Comments(count = 0, canPost = true),
+                likes = Likes(count = 0, usersLike = true, canLike = true, canPublish = true)
+            )
+        )
+    )
+    println(
+        WallService.upDate(
+            Post(
+                1,
+                2,
+                "Hello",
+                "Test2",
+                2,
+                2,
+                2,
+                canPin = false,
+                canEdit = false,
+                comments = Comments(count = 1, canPost = false),
+                likes = Likes(count = 1, usersLike = false, canLike = false, canPublish = false)
+            )
+        )
+    )
+
 }
 
 data class Post(
@@ -12,7 +45,9 @@ data class Post(
     val fromId: Int,
     val createdBy: Int,
     val canPin: Boolean,
-    val canEdit: Boolean
+    val canEdit: Boolean,
+    val comments: Comments = Comments(0, true),
+    val likes: Likes = Likes(0, usersLike = true, canLike = true, canPublish = true)
 )
 
 data class Likes(
@@ -31,14 +66,16 @@ object WallService {
     private var posts = emptyArray<Post>()
     private var id = 1
     fun add(post: Post): Post {
-        posts += post
+        val changePost = post.copy(id = id++)
+        posts += changePost
         return posts.last()
     }
 
     fun upDate(newPost: Post): Boolean {
         for ((index, post) in posts.withIndex()) {
             if (post.id == newPost.id) {
-                posts[index] = newPost.copy(text = post.text)
+                posts[index] = newPost.copy()
+                println(newPost)
                 return true
             }
         }
